9f1ffb1a5f1a623b2a8c60eb54771b0dccb86416
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2019-10-08 14:33:23 +0530

    Maintainance massage deleted

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ app.use((req, res, next) => {
   });
   next();
 });
-app.use((req, res, next) => {
-  res.render("maintenance.hbs");
-});
+
 app.use(express.static(__dirname + "/public"));
 hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
